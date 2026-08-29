a4a1c447ea343d1e3b9e5044bab5bdfdf37730af
Umesh Lahudkar <99596032+umeshlahudkar@users.noreply.github.com>
2026-08-29 16:00:26 +0530

    colors data

--- Assets/Script/Enums/PairColorType.cs
+++ Assets/Script/Enums/PairColorType.cs
@@ -6,14 +6,17 @@ namespace FreeFlow.Enums
     {
         None = 0,
         Red = 1,
-        Blue,
-        Yellow,
-        Green,
-        Orange,
-        Cyan,
-        Voilet,
-        Pink,
-        Purple
+        Blue = 2,
+        Yellow = 3,
+        Green = 4,
+        Orange = 5,
+        Cyan = 6,
+        Indigo = 7,
+        Pink = 8,
+        Purple = 9,
+        Brown = 10,
+        Lime = 11,
+        Teal = 12
     }
 }
 
